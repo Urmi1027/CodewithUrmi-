550e048c628fd2e309a13256e78eed623ba9b922
Sharmin Urmi <105403670+Urmi1027@users.noreply.github.com>
2022-09-18 04:29:56 +0000

    update

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-let marks = {
-  urmi = 90,
-  lovish = 70,
-  monika = 65,
-}
+//let marks = {
+  // urmi = 90,
+  // lovish = 70,
+  // monika = 65,
+//}
+// for(let i=0; i<Object.keys(marks).length;i++){
+  // console.log("the marks of" + Object.keys(marks)[i] + "are" + marks[Object.keys(marks)[i]])
+//}  //
+let cn = 43
+let i
+while (i != cn) {
+  console.log("try again")
+  i = prompt("Enter the number")
   
+}
+console.log("You enter the correct number")
